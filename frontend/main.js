5b4b1dcd33b68f4de32ef5cd78e9ac396ed725cc
shubhtong <shubh.jain1993@gmail.com>
2022-09-07 10:48:03 +0530

    added deployed func URL

--- frontend/main.js
+++ frontend/main.js
@@ -2,10 +2,11 @@ window.addEventListener("DOMContentLoaded", (event) => {
     getViewConuter();
 });
 
-const apiPath = "http://localhost:7071/api/GetResumeCounter";
+const apiPath_Deployed = "https://getreusmecounterazure.azurewebsites.net/api/GetResumeCounter?code=gMXXRvabPzmjTPxqa1324kIxeS411cTdYf2zZR2ktQ_-AzFu-q-BOg==";
+const apiPath_Local = "http://localhost:7071/api/GetResumeCounter";
 
 const getViewConuter = () => {
-    fetch(apiPath).then(response => {
+    fetch(apiPath_Deployed).then(response => {
         return response.json();
     }).then(response => {
         console.log("API called to get counter!");
